9cfa5cc275fda97fb68074242e1c93aaafa96f74
riyanandakumar <68032343+riyanandakumar@users.noreply.github.com>
2020-08-03 22:51:21 -0400

    Add files via upload

---
+++ Failed App Environments/Swing Classifier 2/Swing Classifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swing Classifier
+//
+//  Created by Namita Nandakumar on 7/31/20.
+//  Copyright © 2020 Riya Nandakumar. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+    
+    func predictSwing(){
+        let model = Swing_Classifier_1()
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Failed App Environments/Swing Classifier/Swing Classifier/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Swing Classifier
+//
+//  Created by Namita Nandakumar on 7/31/20.
+//  Copyright © 2020 Riya Nandakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
